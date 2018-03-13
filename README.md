07b0f4214f7c0a7a8ea4cb15de20de1f35ef8ed7
Jose Alvarado <jose.alvarado2020@gmail.com>
2018-03-14 01:09:10 +0800

    gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+My Trade Coin Contract
+
+Documentation coming soon...
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,26 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+
+    QA: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3" ,  // ROPSTEN
+      // Options - gas, gasPrice, from
+      from: "0x3a75ad8d08b388ca0f2afafb3c092aaf40ae72a6",
+      gas: 4600000
+    },
+
+    PRODUCTION: {
+      host: "localhost",
+      port: 8545,
+      network_id: "1"   // LIVE
+      // Options - gas, gasPrice, from
+    }
+  }
 };
+
